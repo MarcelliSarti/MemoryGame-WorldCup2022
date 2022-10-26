a9fca0f60d3edc1ebc2148a1bfc5b8e6e33315cd
Wallyson Jhonatan <wallyson03jhonatan@gmail.com>
2022-10-26 19:33:25 -0300

    js

--- js/home.js
+++ js/home.js
@@ -1,3 +1,53 @@
+var modoJogo = 'classico';
+var tamanhoTabuleiro = 2;
+let selectCards = [];
+let qtdCards = 0;
+
+const cards = document.querySelector(".cards");
+const modal = document.getElementById("modal");
+
+const cardImages = ['qat', 'ecu', 'sen', 'ned', 'eng', 'irn', 'usa', 'wal', 'arg',
+  'ksa', 'mex', 'pol', 'fra', 'aus', 'den', 'tun', 'esp', 'crc', 'ger', 'jpn', 'bel',
+  'can', 'mar', 'cro', 'bra', 'srb', 'sui', 'cmr', 'por', 'gha', 'uru', 'kor'];
+
+function getValueModoJogo(modoJogoSelect) {
+    if (modoJogo != '') {
+        var optionModoJogoDeActivated = document.getElementById(modoJogo);
+        optionModoJogoDeActivated.classList.remove("active");
+    }
+    modoJogo = modoJogoSelect;
+    var optionModoJogoActivated = document.getElementById(modoJogoSelect);
+    optionModoJogoActivated.classList.add("active");
+}
+
+function getValueTamanhoTabuleiro(tamanhoTabuleiroSelect) {
+    if (tamanhoTabuleiro != '') {
+        var optionTamanhoTabuleiroDeActivated = document.getElementById(tamanhoTabuleiro);
+        optionTamanhoTabuleiroDeActivated.classList.remove("active");
+    }
+    tamanhoTabuleiro = tamanhoTabuleiroSelect;
+    var optionTamanhoTabuleiroActivated = document.getElementById(tamanhoTabuleiro);
+    optionTamanhoTabuleiroActivated.classList.add("active");
+}
+
+function startGame() {
+    if(modoJogo === ''){
+        alert("Selecione o modo de jogo para prosseguir!!!");
+        return;
+    }
+    if(tamanhoTabuleiro === 0){
+        alert("Selecione o tamanho do tabuleiro para prosseguir!!!");
+        return;
+    }
+
+    modal.style.display = "none";
+
+    qtdCards = (tamanhoTabuleiro * tamanhoTabuleiro) / 2;
+    selectCards = cardImages.slice(0,qtdCards);
+    
+    loadGame();
+}
+
 const showFace = ({target}) => {
     console.log(target.parentNode.classList);
     if (target.classList == 'back face') {
@@ -10,6 +60,49 @@ const showFace = ({target}) => {
     target.parentNode.classList.add('showFace');
 }
 
+function createImageElement(url) {
+    const image = document.createElement('img');
+    image.src = url;
+    return image;
+}
+
+function createElement(tag, className){
+    const element = document.createElement(tag);
+    element.className = className;
+    return element;
+}
+
+function createCard(cardImage) {
+    const card = createElement('div', 'card');
+    const front = createElement('div', 'face front');
+    const back = createElement('div', 'face back');
+    
+    const frontImage = createImageElement(`./img/bandeiras/${cardImage}.png`);
+    front.appendChild(frontImage);
+
+    const backImage = createImageElement(`./img/miniLogo.png`);
+    back.appendChild(backImage);
+
+    card.appendChild(front);
+    card.appendChild(back);
+
+    card.addEventListener('click', showFace);
+
+    card.setAttribute('data-image', cardImage);
+    return card;
+}
+
+function loadGame() {
+    cards.style.gridTemplateColumns = 'repeat(' + tamanhoTabuleiro + ',1fr)';
+    const duplicateImages = [...selectCards,...selectCards];
+    const sortDuplicateImages = duplicateImages.sort(() => Math.random() - 0.5);
+
+    sortDuplicateImages.forEach((cardImage)=> {
+        const card = createCard(cardImage);
+        cards.appendChild(card);
+    })
+}
+
 // function loadCards(){
 //     alert('oi');
 //     for(let i=0;i<4;i++){
@@ -18,6 +111,6 @@ const showFace = ({target}) => {
 //     }
 // }
 
-const card = document.getElementById('show');
+// const card = document.getElementById('show');
 
-card.addEventListener('click', showFace);
+// card.addEventListener('click', showFace);
